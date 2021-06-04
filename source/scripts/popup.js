b7bc9bef5c788ac920f4246c58d6931cebc49b74
Prithviraj Sukale <prithviraj.sukale@vineti.com>
2021-06-04 17:43:48 +0530

    Fixed the sign in message for login

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -4,7 +4,12 @@ import '../styles/popup.scss';
 var envConfig = require('./constants')
 
 function openWebPage(url) {
-  return browser.tabs.create({url});
+  return browser.tabs.create({ url });
+}
+
+function showMessage(message) {
+  document.getElementById('button').textContent = message;
+  document.getElementById('spinner').classList.add("hidden");
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -14,8 +19,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   //   url,
   // });
 
-   console.log(envConfig.BASE_URL);
-
   document.getElementById('button').addEventListener('click', async (event) => {
     document.getElementById('spinner').classList.remove("hidden");
 
@@ -23,12 +26,28 @@ document.addEventListener('DOMContentLoaded', async () => {
       active: true,
       currentWindow: true
     });
-  
+
     const url = tabs.length && tabs[0].url;
     const title = tabs.length && tabs[0].title;
-    axios.post(`${envConfig.BASE_URL}/add-to-board`, { url: url, title: title}).then((response)=>{
-      document.getElementById('button').textContent = "Job added ✅";
-      document.getElementById('spinner').classList.add("hidden");
+    axios.post(`${envConfig.BASE_URL}/add-to-board`, { url: url, title: title })
+    .then((response) => {
+      showMessage("Job added ✅");
+    }).catch(err => {
+      if (err.response) {
+        if (err.response.status == 401) {
+          showMessage("Please sign in first");
+          openWebPage(`${envConfig.BASE_URL}/users/sign_in`);
+        }
+        else {
+          showMessage("There was an error, please retry or click on help");
+        }
+      } else if (err.request) {
+        // client never received a response, or request never left
+        showMessage("There was an error, please retry or click on help");
+      } else {
+        // anything else
+        showMessage("There was an error, please retry or click on help");
+      }
     })
   });
 });
